3687a7ff0122e1a1cdcf816834cbda5f0f9fa412
jimmy <jimmylew9779@gmail.com>
2023-03-26 22:08:09 +0800

    feat: complete results page

---
+++ web/components/Card.vue
@@ -0,0 +1,60 @@
+<script lang="ts">
+export default {
+	inheritAttrs: false,
+}
+</script>
+
+<script setup lang="ts">
+const { note, expand, isCompact } = withDefaults(defineProps<{
+	title: string
+	value: string | number
+	icon: string
+	note?: [string | number, string, string] // [value, unit, icon]
+	isCompact?: boolean
+	expand?: boolean
+}>(), {
+	isCompact: false,
+	expand: false,
+})
+
+const stat_class = computed(() => {
+	return [
+		isCompact ? 'text-lg' : 'text-3xl',
+		!note ? 'mt-5' : isCompact ? 'mt-2' : 'mt-1',
+	]
+})
+</script>
+
+<template>
+	<div
+		class="
+		flex flex-col
+		rounded-2xl h-52 p-6
+		my-shadow bg-white"
+		:class="`${expand ? 'min-w-max' : 'aspect-square'}`"
+	>
+		<div class="inline-flex items-center justify-start mb-2">
+			<div class="flex items-center justify-center w-8 h-8 p-1 rounded-lg" :class="$attrs.class">
+				<Icon size="20" :name="icon" class="text-white" />
+			</div>
+		</div>
+		<h3 class="font-semibold text-sm mt-2 mb-1">
+			{{ title }}
+		</h3>
+		<h1 class="font-bold" :class="stat_class">
+			{{ value }}
+		</h1>
+		<div v-if="note" class="mb-2 flex items-center justify-start text-zinc-400 text-xs" :class="`${isCompact ? 'mt-4' : 'mt-3'}`">
+			<Icon v-if="note[2]" size="16" :name="note[2]" class="mr-1" />
+			<p class="text-center mt-0.5">
+				<span class="font-bold">{{ note[0] }}</span> {{ note[1] }}
+			</p>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+.my-shadow {
+	box-shadow: 0px 22px 56px rgba(180, 155, 190, 0.22);
+}
+</style>
---
+++ web/pages/results.vue
@@ -0,0 +1,171 @@
+<script setup lang="ts">
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+const { data: time } = await useFetch<TimeResponse>('http://localhost:8080/get_time')
+const { data: trip } = await useFetch<TripResponse>('http://localhost:8080/get_trip')
+const { data: user } = await useFetch<UserResponse>('http://localhost:8080/get_user')
+</script>
+
+<template>
+	<div class="">
+		<div class="relative pt-8 overflow-y-hidden">
+			<div
+				class="
+				absolute z-0 inset-0
+				bg-gradient-to-b from-purple-200/50 to-white
+				w-full h-48"
+			/>
+			<div class="flex flex-col mt-10 w-full">
+				<h1 class="relative z-10 font-bold text-2xl mb-4 pl-16">
+					Time Data
+				</h1>
+				<div
+					class="
+					relative z-10
+					flex items-center justify-start
+					w-full overflow-x-scroll gap-10
+					px-16 pb-16"
+				>
+					<Card
+						class="bg-emerald-500"
+						title="Favourite hour"
+						icon="bx:hourglass"
+						:value="`${time?.hour.value}:00`"
+						:note="[time?.hour.count || 0, 'users', 'material-symbols:person-3-rounded']"
+					/>
+					<Card
+						class="bg-amber-500"
+						title="Favourite day"
+						icon="material-symbols:calendar-view-day-rounded"
+						:value="time?.day.value.toString().slice(0, 3) || ''"
+						:note="[time?.day.count || 0, 'users', 'material-symbols:person-3-rounded']"
+					/>
+					<Card
+						class="bg-rose-500"
+						:value="months[(time?.month.value || 1) - 1]"
+						title="Favourite month"
+						icon="fluent:calendar-month-20-filled"
+						:note="[time?.month.count || 0, 'users', 'material-symbols:person-3-rounded']"
+					/>
+				</div>
+			</div>
+			<div class="flex flex-col w-full">
+				<h1 class="relative z-10 font-bold text-2xl mb-4 pl-16">
+					Trip Data
+				</h1>
+				<div
+					class="
+					relative z-10
+					flex items-center justify-start
+					overflow-x-scroll gap-10
+					px-16 pb-16"
+				>
+					<Card
+						class="bg-violet-500"
+						title="Favourite route"
+						icon="ic:twotone-route"
+						:value="trip?.full_trip || ''"
+						:note="[trip?.count || 0, 'users', 'material-symbols:person-3-rounded']"
+						is-compact
+						expand
+					/>
+					<Card
+						class="bg-orange-500"
+						title="Favourite start station"
+						icon="material-symbols:line-start-circle-outline-rounded"
+						:value="trip?.station.start.value || ''"
+						:note="[trip?.station.start.count || 0, 'users', 'material-symbols:person-3-rounded']"
+						is-compact
+						:expand="(trip?.station.start.value.length || 0) > 12"
+					/>
+					<Card
+						class="bg-sky-500"
+						title="Favourite end station"
+						icon="material-symbols:line-end-circle-outline-rounded"
+						:value="trip?.station.end.value || ''"
+						:note="[trip?.station.end.count || 0, 'users', 'material-symbols:person-3-rounded']"
+						is-compact
+						:expand="(trip?.station.start.value.length || 0) > 12"
+					/>
+					<Card
+						class="bg-emerald-500"
+						title="Average duration"
+						icon="bx:hourglass"
+						:value="`${Math.round((trip?.duration.avg || 0) / 60)} min`"
+					/>
+					<Card
+						class="bg-red-600"
+						title="Total hours ridden"
+						icon="bx:hourglass"
+						:value="Math.round((trip?.duration.total || 0) / 3600)"
+					/>
+				</div>
+			</div>
+			<div class="flex flex-col">
+				<h1 class="relative z-10 font-bold text-2xl mb-4 pl-16">
+					User Data
+				</h1>
+				<div
+					class="
+					relative z-10
+					flex items-center justify-start
+					w-full overflow-x-scroll gap-10
+					px-16 pb-16"
+				>
+					<Card
+						class="bg-amber-500"
+						title="Subscribers"
+						icon="ph:star-four-bold"
+						:value="user?.type.subscriber || 0"
+					/>
+					<Card
+						class="bg-indigo-500"
+						title="Customers"
+						icon="material-symbols:person-3-rounded"
+						:value="user?.type.customer || 0"
+					/>
+					<Card
+						v-if="user?.gender"
+						class="bg-sky-500"
+						title="Male users"
+						icon="ic:round-male"
+						:value="user.gender.male"
+					/>
+					<Card
+						v-if="user?.gender"
+						class="bg-pink-500"
+						title="Female users"
+						icon="ic:round-female"
+						:value="user.gender.female"
+					/>
+					<Card
+						v-if="user?.birth"
+						class="bg-rose-500"
+						title="Most common birth year"
+						icon="bx:hourglass"
+						:value="user.birth.mode"
+						expand
+					/>
+					<Card
+						v-if="user?.birth"
+						class="bg-violet-500"
+						title="Oldest user"
+						icon="bx:hourglass"
+						:value="user.birth.earliest"
+					/>
+					<Card
+						v-if="user?.birth"
+						class="bg-orange-500"
+						title="Youngest user"
+						icon="bx:hourglass"
+						:value="user.birth.latest"
+					/>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+
+</style>
